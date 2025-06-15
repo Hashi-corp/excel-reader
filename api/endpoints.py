75a119e24c399418f59ac3008beb66ad333bd355
Hashi-corp <harishkrishnan.p2022@vitstudent.ac.in>
2025-06-15 20:05:02 +0530

    updated endpoints

--- api/endpoints.py
+++ api/endpoints.py
@@ -10,13 +10,11 @@ router = APIRouter()
 @router.post("/upload", response_model=FileUploadResponse)
 async def upload_file(file: UploadFile =  File(...)):
     #input the file from user and validate file type
-       
     return FileUploadResponse(file_name=file.filename, message="File uploaded successfully")
 
 @router.post("/plot", response_model=UserPromptResponse)
 async def user_prompt(prompt: str):
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt cannot be empty")
-    
     # Process the the prompt via LLM and generate appropriate plots
     return UserPromptResponse(prompt=prompt)
